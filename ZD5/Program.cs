d1690669ab63949758bb37a850c66be3404e0d93
Zemlyanova-Victoria <zemlyanova-v@list.ru>
2020-05-29 12:58:29 +0500

    first commit

---
+++ ZD5/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using static System.Convert;
+using static System.Console;
+using System.Collections.Generic;
+
+
+namespace ZD5
+{
+    class bel
+
+    {
+        public class Student
+
+        {
+            public string ID;
+
+            public string FIO;
+
+            public string GROUP;
+
+            public string DATA;
+
+        }
+
+        public List<Student> list = new List<Student>();
+
+        public void add(string ID, string FIO, string GROUP, string DATA)
+
+        {
+            list.Add(new Student() { ID = ID, FIO = FIO, GROUP = GROUP, DATA = DATA });
+        }
+
+        public void remov(string ID)
+
+        {
+            for (int i = 0; i < list.Count; i++)
+
+            {
+                if (list[i].ID == ID) list.RemoveAt(i);
+            }
+        }
+
+        public void change(string ID, string FIO, string GROUP, string DATA)
+
+        {
+            for (int i = 0; i < list.Count; i++)
+
+            {
+                if (list[i].ID == ID)
+
+                {
+                    list[i].FIO = FIO;
+
+                    list[i].GROUP = GROUP;
+
+                    list[i].DATA = DATA;
+
+                }
+
+            }
+
+        }
+
+        public void show()
+
+        {
+            foreach (var i in list)
+
+            {
+                Console.WriteLine(i.ID + " " + i.FIO + " " + i.GROUP + " " + i.DATA);
+            }
+
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bel a = new bel();
+
+            WriteLine("1 - Добавить студента.\n2 - Изменить данные о студенте.\n3 - Удалить студента.\n4 - Вывод всех студентов.");
+
+            int n = ToInt32(ReadLine());
+
+            while (n > 0)
+
+            {
+
+                if (n == 1)
+
+                {
+                    WriteLine("ID: "); string ID = ReadLine();
+
+                    WriteLine("ФИО: "); string FIO = ReadLine();
+
+                    WriteLine("Группа: "); string GROUP = ReadLine();
+
+                    WriteLine("Дата: "); string DATA = ReadLine();
+
+                    a.add(ID, FIO, GROUP, DATA);
+
+                    WriteLine("Введи действие: ");
+
+                    n = ToInt32(ReadLine());
+
+                }
+
+                else if (n == 2)
+
+                {
+                    WriteLine("Введи ID и измененные данные: ");
+
+                    WriteLine("ID: "); string ID = ReadLine();
+
+                    WriteLine("ФИО: "); string FIO = ReadLine();
+
+                    WriteLine("Группа: "); string GROUP = ReadLine();
+
+                    WriteLine("Дата: "); string DATA = ReadLine();
+
+                    a.change(ID, FIO, GROUP, DATA);
+
+                    WriteLine("Введи действие: ");
+
+                    n = ToInt32(ReadLine());
+
+                }
+
+                else if (n == 3)
+
+                {
+                    WriteLine("Введи ID: ");
+
+                    string ID = ReadLine();
+
+                    a.remov(ID);
+
+                    WriteLine("Введи действие: ");
+
+                    n = ToInt32(ReadLine());
+
+                }
+
+                else if (n == 4)
+
+                {
+                    a.show();
+
+                    WriteLine("Введи действие: ");
+
+                    n = ToInt32(ReadLine());
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+        
+    
+        
+    
+
